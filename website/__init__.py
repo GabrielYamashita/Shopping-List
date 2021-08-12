8714c125fd250a199ed3f865b31f1bd0fb16e857
Gabriel Yamashita <gabriel.yamashita01@gmailcom>
2021-08-12 17:29:42 -0300

    sla

--- website/__init__.py
+++ website/__init__.py
@@ -6,9 +6,8 @@ from os import path
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-
+app = Flask(__name__)
 def create_app():
-    app = Flask(__name__)
     app.config['SECRET_KEY'] = 'yama'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
